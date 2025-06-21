87ef7cedc0b56eb44be915a1741a8a055ac37b00
Sakshi Patil <sashpatil11@gmail.com>
2025-06-21 11:50:46 +0530

    Delete routes directory

--- routes/quest.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTodayQuest } = require('../controllers/questController');
-const auth = require('../middleware/auth');
-
-router.get('/today', auth, getTodayQuest);
-
-module.exports = router;
